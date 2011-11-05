c4e30df880d56a35aced20c6cc7c95ac97c83707
Corey Innis <corey@coolerator.net>
2011-11-05 10:24:11 -0700

    restores clobbered response-code

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -7,7 +7,7 @@ module Remotipart
     def render *args
       super
       if remotipart_submitted?
-        response.body = %{<textarea data-type=\"#{content_type}\">#{escape_once(response.body)}</textarea>}
+        response.body = %{<textarea data-type=\"#{content_type}\" response-code=\"#{response.response_code}\">#{escape_once(response.body)}</textarea>}
         response.content_type = Mime::HTML
       end
       response_body
