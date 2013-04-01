879d5903e1494c18bf0b94ed49e57fa244e44cab
yasirs <yasir.suhail@gmail.com>
2013-04-01 17:56:05 -0300

    Fixes issue #71

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -2,7 +2,7 @@ module Remotipart
   # Responder used to automagically wrap any non-xml replies in a text-area
   # as expected by iframe-transport.
   module RenderOverrides
-    include ActionView::Helpers::TagHelper
+    include ERB::Util
 
     def self.included(base)
       base.class_eval do
@@ -13,7 +13,7 @@ module Remotipart
     def render_with_remotipart *args
       render_without_remotipart *args
       if remotipart_submitted?
-        response.body = %{<textarea data-type=\"#{content_type}\" response-code=\"#{response.response_code}\">#{escape_once(response.body)}</textarea>}
+        response.body = %{<textarea data-type=\"#{content_type}\" response-code=\"#{response.response_code}\">#{html_escape(response.body)}</textarea>}
         response.content_type = Mime::HTML
       end
       response_body
