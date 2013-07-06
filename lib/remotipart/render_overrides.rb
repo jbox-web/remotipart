7e350e8f311f32ba638a5db63862d59b23ec5309
Steve Schwartz <steve@alfajango.com>
2013-07-06 11:36:02 -0400

    Fixed issue with json parsing error. Only html_escape html responses. Fixes #85.

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -13,7 +13,8 @@ module Remotipart
     def render_with_remotipart *args
       render_without_remotipart *args
       if remotipart_submitted?
-        response.body = %{<textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{html_escape(response.body)}</textarea>}
+        textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
+        response.body = %{<textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{textarea_body}</textarea>}
         response.content_type = Mime::HTML
       end
       response_body
