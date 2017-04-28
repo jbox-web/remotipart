e0c2731d51e7cf03e34ee618a5901c159dc29322
Weston Ganger <westonganger@gmail.com>
2017-04-28 11:01:06 -0700

    fix #89

--- lib/remotipart/view_helper.rb
+++ lib/remotipart/view_helper.rb
@@ -1,9 +1,20 @@
 module Remotipart
   module ViewHelper
+
+    def escape_javascript(javascript)
+      if remotipart_submitted?
+        super("#{javascript}")
+      else
+        super
+      end
+    end
+    alias_method :j, :escape_javascript
+
     #No longer used
     #Retrained to prevent issues while updating
     def remotipart_response(options = {}, &block)
       with_output_buffer(&block)
     end
+
   end
 end
