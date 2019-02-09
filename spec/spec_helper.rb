da9e30ff26b1a0806ca6363860ccc4e2788cd949
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-09 21:01:49 +0100

    Improve RSpec config, replace Poltergeist by Selenium

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,29 +4,60 @@ require File.expand_path('../dummy_app/config/environment', __FILE__)
 
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'capybara/poltergeist'
 require 'database_cleaner'
+require 'selenium-webdriver'
 
-Capybara.javascript_driver = :poltergeist
-Capybara.default_max_wait_time = 5
+def register_driver(driver_name, args = [])
+  Capybara.register_driver(driver_name) do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--window-size=1920,1200')
+    args.each do |arg|
+      options.add_argument(arg)
+    end
+
+    Capybara::Selenium::Driver.new(app,
+      browser: :chrome,
+      options: options
+    )
+  end
+end
+
+# Register our own custom drivers
+register_driver(:chrome)
+register_driver(:headless_chrome, %w[headless disable-gpu no-sandbox disable-dev-shm-usage])
+
+# Configure Capybara JS driver
+Capybara.current_driver    = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
+
+# Configure Capybara server
+Capybara.run_server = true
+Capybara.server     = :puma, { Silent: true }
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f }
 
 RSpec.configure do |config|
-  load "#{Rails.root.to_s}/db/schema.rb" # use db agnostic schema by default
-
-  config.mock_with :rspec
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-  config.fixture_path = File.expand_path('../fixtures', __FILE__)
-
   config.include Rails.application.routes.url_helpers
   config.include RSpec::Matchers
   config.include Capybara::DSL, type: :feature
   config.include IntegrationHelper, type: :feature
+
+  load "#{Rails.root.to_s}/db/schema.rb" # use db agnostic schema by default
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.mock_with :rspec
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.use_transactional_fixtures = false
+
+  config.fixture_path = File.expand_path('../fixtures', __FILE__)
 
   config.before do |example|
     DatabaseCleaner.strategy = :truncation
