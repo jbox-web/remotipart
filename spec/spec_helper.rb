e829151a355b9fea01d3618bfc83f83034945430
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-11-12 00:39:28 +0100

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,7 @@ require File.expand_path('dummy/config/environment.rb', __dir__)
 
 # Load test gems
 require 'rspec/rails'
-require 'rspec/retry'
-require 'capybara/rspec'
 require 'capybara/cuprite'
-require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
